a97c7745dfbe6d3b0001e13d53c7748ab4cb40ce
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-05-15 23:52:42 +0200

    basic project styles

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <img className='app__logo' src={logo} alt="" />
+      <div className='app__container'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
