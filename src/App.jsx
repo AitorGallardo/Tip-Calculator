3829cc1fcc249fb5208ec576fa6033e8929044c3
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-05-24 22:42:27 +0200

    getting results and formatting inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,65 @@
 import logo from './assets/logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { formatMaxNumberLength } from './helpers/formatMaxNumberLength';
 
 function App() {
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState('');
+  const [numberOfPeople, setNumberOfPeople] = useState('');
   const [validBill, setValidBill] = useState(true);
   const [validNumberOfPeople, setValidNumberOfPeople] = useState(true);
+  const [tipPerPerson, setTipPerPerson] = useState(0);
+  const [totalPerPerson, setTotalPerPerson] = useState(0);
 
-  const handleValidInput = (e) => {
+  useEffect(() => {
+    if (bill && tip && numberOfPeople) {
+      const { tipPerson, totalPerson } = calcTip({ bill, tip, numberOfPeople });
+      setTipPerPerson(tipPerson);
+      setTotalPerPerson(totalPerson);
+    }
+  }, [bill, tip, numberOfPeople]);
+
+  const handleInput = (e) => {
     const value = e.currentTarget.value;
     const name = e.currentTarget.name;
 
     if (name === 'bill') {
+      const formatedValue = formatMaxNumberLength(value,5)
       setValidBill(true);
-      if (value <= 0) {
+      setBill(formatedValue);
+      if (formatedValue <= 0) {
         setValidBill(false);
       }
     }
     if (name === 'people') {
+      const formatedValue = formatMaxNumberLength(value,2)
       setValidNumberOfPeople(true);
-      if (value <= 0) {
+      setNumberOfPeople(formatedValue);
+      if (formatedValue <= 0) {
         setValidNumberOfPeople(false);
       }
     }
+  };
+
+  const handleSelectTip = (e) => {
+    const tipValue = e.target.id;
+    setTip(tipValue);
+  };
+
+  const calcTip = ({ bill, tip, numberOfPeople }) => {
+    bill = Number(bill);
+    numberOfPeople = Number(numberOfPeople);
+    numberOfPeople = Number(numberOfPeople);
+    
+    const totalTip = bill * (tip / 100);
+
+    const n1 = totalTip / numberOfPeople;
+    const n2 = (bill + totalTip) / numberOfPeople;
+    const tipPerson = Math.round(n1 * 100) / 100;
+    const totalPerson = Math.round(n2 * 100) / 100;
+
+    return { tipPerson, totalPerson };
   };
 
   return (
@@ -43,37 +81,60 @@ function App() {
                   className={!validBill ? 'invalid-input' : ''}
                   name='bill'
                   id='bill'
-                  type='number'
+                  type='text'
                   placeholder='0'
-                  onChange={handleValidInput}
+                  max="99999"
+                  value={bill}
+                  onChange={handleInput}
                 />
               </div>
             </fieldset>
             <div className='app__tip-select'>
               <label htmlFor='selector'>Select Tip %</label>
               <div id='selector' className='app__tip-grid'>
-                <button role='option' className='app__tip-option'>
+                <button
+                  id='5'
+                  role='option'
+                  className={`app__tip-option ${tip == 5 ? 'selected' : ''}`}
+                  onClick={handleSelectTip}
+                >
                   5%
                 </button>
-                <button role='option' className='app__tip-option'>
+                <button
+                  id='10'
+                  role='option'
+                  className={`app__tip-option ${tip == 10 ? 'selected' : ''}`}
+                  onClick={handleSelectTip}
+                >
                   10%
                 </button>
-                <button role='option' className='app__tip-option'>
+                <button
+                  id='15'
+                  role='option'
+                  className={`app__tip-option ${tip == 15 ? 'selected' : ''}`}
+                  onClick={handleSelectTip}
+                >
                   15%
                 </button>
                 <button
+                  id='25'
                   role='option'
-                  aria-selected='true'
-                  className='app__tip-option'
+                  className={`app__tip-option ${tip == 25 ? 'selected' : ''}`}
+                  onClick={handleSelectTip}
                 >
                   25%
                 </button>
-                <button role='option' className='app__tip-option'>
+                <button
+                  id='50'
+                  role='option'
+                  className={`app__tip-option ${tip == 50 ? 'selected' : ''}`}
+                  onClick={handleSelectTip}
+                >
                   50%
                 </button>
                 <input
                   className='app__tip-custom'
-                  type='number'
+                  type='text'
                   placeholder='Custom'
                 />
               </div>
@@ -93,7 +154,8 @@ function App() {
                   id='people'
                   type='number'
                   placeholder='0'
-                  onChange={handleValidInput}
+                  value={numberOfPeople}
+                  onChange={handleInput}
                 />
               </div>
             </fieldset>
@@ -105,14 +167,14 @@ function App() {
                   <p className='app_result-value-text-top'>Tip Amount</p>
                   <p className='app_result-value-text-sub'>/person</p>
                 </div>
-                <p className='app_result-value-value'>$0.00</p>
+                <p className='app_result-value-value'>{tipPerPerson}</p>
               </div>
               <div className='app__result-value total'>
                 <div className='app_result-value-text'>
                   <p className='app_result-value-text-top'>Total</p>
                   <p className='app_result-value-text-sub'>/person</p>
                 </div>
-                <p className='app_result-value-value'>$0.00</p>
+                <p className='app_result-value-value'>{totalPerPerson}</p>
               </div>
             </div>
 
---
+++ src/helpers/formatMaxNumberLength.js
@@ -0,0 +1,6 @@
+export const formatMaxNumberLength = (value, maxNumber = 2) => {
+    // Remove all non-digit characters from the input string
+    value = value.replace(/\D/g, '');
+    return value.length > maxNumber ? value.slice(0, maxNumber) : value;
+  };
+  
